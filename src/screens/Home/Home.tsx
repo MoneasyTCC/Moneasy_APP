9d526abc2ee490ebc609f05af8347d48168d1a8f
Felipe Oliveira <felipeol.est@gmail.com>
2024-03-03 22:17:14 -0300

    adicionar transacao

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { View, Text, Button, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, Image, Modal, TextInput, Alert } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../../shared/config";
 import { Shadow } from "react-native-shadow-2";
 import { adicionarNovaMeta } from '../../../services/testeBanco';
 import { adicionarNovaTransacao } from '../../../services/testeBanco';
+import DatePicker from "react-native-datepicker";
+import { Transacao } from "../../../Model/Transacao";
+import { TransacaoDAL } from "../../../Repo/RepositorioTransacao";
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -15,50 +18,109 @@ type Props = {
   navigation: HomeScreenNavigationProp;
 };
 
-const adicionarMeta = async () => {
-  await adicionarNovaMeta();
-};
-
-const adicionarTran = async () => {
-  await adicionarNovaTransacao();
-};
 
 // Use as props na definição do seu componente
 export default function HomeScreen({ navigation }: Props) {
+
+  var [isModalVisible, setModalVisible] = useState(false);
+  const [valor, setValor] = useState('');
+  const [data, setData] = useState(new Date());
+  const [descricao, setDescricao] = useState('');
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  const novosDados: Transacao = {
+    id:"",
+    usuarioId: "",
+    tipo:"entrada",
+    valor: parseFloat(valor),
+    data:(data),
+    descricao:(descricao),
+    moeda:"BRL"
+  };
+  const handleTransacao = async () => {
+    try{
+      TransacaoDAL.adicionarTransacao(novosDados)
+      Alert.alert("Transação adicionada com Sucesso!");
+
+    }
+    catch(err){
+    Alert.alert("Erro ao adicionar transação");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.menuHeader}>
-        <Button title="Sair" onPress={() => navigation.replace("Inicio")} />
-        <Button title="AdicionarMeta" onPress={adicionarMeta} />
-        <Button title="AdicionarTransacao" onPress={adicionarTran} />
+    <View style={styles.menuHeader}>
+      <Button title="Sair" onPress={() => navigation.replace("Inicio")} />
+      <Button title="Adicionar Transação" onPress={toggleModal} />
 
-      </View>
-      <View style={styles.menuBody}>
-        <View style={styles.content}></View>
-      </View>
-      <View style={styles.menuFooter}>
-        <View style={styles.menuNavegation}>
-          <Text></Text>
-          <Image
-            style={styles.img}
-            source={require("../../../assets/menu/homeActive.png")}
-          />
-          <Image
-            style={styles.img}
-            source={require("../../../assets/menu/menu.png")}
-          />
-          <Image
-            style={styles.img}
-            source={require("../../../assets/menu/transactions.png")}
-          />
-          <Image
-            style={styles.img}
-            source={require("../../../assets/menu/more.png")}
-          />
-          <Text></Text>
-        </View>
+<Modal visible={isModalVisible}>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <TextInput
+      placeholder="Valor"
+      keyboardType="numeric"
+      value={valor}
+      onChangeText={setValor}
+    />
+    <DatePicker
+      style={{ width: 200 }}
+      date={data}
+      mode="date"
+      placeholder="Selecione a data"
+      format="YYYY-MM-DD"
+      confirmBtnText="Confirmar"
+      cancelBtnText="Cancelar"
+      customStyles={{
+        dateIcon: {
+          position: 'absolute',
+          left: 0,
+          top: 4,
+          marginLeft: 0,
+        },
+        dateInput: {
+          marginLeft: 36,
+        },
+      }}
+      onDateChange={(dateStr, date) => setData(new Date(dateStr))}
+    />
+    <TextInput
+      placeholder="Descrição"
+      value={descricao}
+      onChangeText={setDescricao}
+    />
+    <Button title="Adicionar" onPress={handleTransacao} />
+    <Button title="Cancelar" onPress={toggleModal} />
+  </View>
+</Modal>
+    </View>
+    <View style={styles.menuBody}>
+      <View style={styles.content}></View>
+    </View>
+    <View style={styles.menuFooter}>
+      <View style={styles.menuNavegation}>
+        <Text></Text>
+        <Image
+          style={styles.img}
+          source={require("../../../assets/menu/homeActive.png")}
+        />
+        <Image
+          style={styles.img}
+          source={require("../../../assets/menu/menu.png")}
+        />
+        <Image
+          style={styles.img}
+          source={require("../../../assets/menu/transactions.png")}
+        />
+        <Image
+          style={styles.img}
+          source={require("../../../assets/menu/more.png")}
+        />
+        <Text></Text>
       </View>
     </View>
+  </View>
   );
 }
 
@@ -116,3 +178,5 @@ const styles = StyleSheet.create({
   },
   img: {},
 });
+
+
