08d6ebcecfb8c2eef0791b820a0e6a2a1140f2c9
joaogvt <sotorivarossi@gmail.com>
2024-03-21 23:02:38 -0300

    mudanças na estrutura do modal e estilos

--- Components/listaTransacao.tsx
+++ Components/listaTransacao.tsx
@@ -26,6 +26,9 @@ const ListaDeTransacoes: React.FC<ListaDeTransacoesProps> = ({
     null
   );
   const [selectedItemTipo, setSelectedItemTipo] = useState<string | null>(null);
+  const [selectedItemDescricao, setSelectedItemDescricao] = useState<
+    string | null
+  >(null);
 
   useEffect(() => {
     const buscarTransacoes = async () => {
@@ -45,9 +48,14 @@ const ListaDeTransacoes: React.FC<ListaDeTransacoesProps> = ({
     buscarTransacoes();
   }, [dataSelecionada]);
 
-  const toggleModal = (itemValue: number, itemTipo: string) => {
+  const toggleModal = (
+    itemValue: number,
+    itemTipo: string,
+    itemDescricao: string
+  ) => {
     setSelectedItemValue(itemValue);
     setSelectedItemTipo(itemTipo);
+    setSelectedItemDescricao(itemDescricao);
     setModalVisible(!isModalVisible);
   };
 
@@ -55,7 +63,9 @@ const ListaDeTransacoes: React.FC<ListaDeTransacoesProps> = ({
     return tipo === "entrada" ? styles.valueEntrada : styles.valueSaida;
   };
   const renderItem = ({ item }: { item: Transacao }) => (
-    <TouchableOpacity onPress={() => toggleModal(item.valor, item.tipo)}>
+    <TouchableOpacity
+      onPress={() => toggleModal(item.valor, item.tipo, item.descricao)}
+    >
       <View style={styles.container}>
         {/* <View style={styles.icon}>{}</View> */}
         <Text style={styles.text}>{item.tipo}</Text>
@@ -79,12 +89,33 @@ const ListaDeTransacoes: React.FC<ListaDeTransacoesProps> = ({
       <Modal visible={isModalVisible} animationType="slide" transparent={true}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text>Selected Item Value: {selectedItemValue}</Text>
-            <Text>Selected Item Tipo: {selectedItemTipo}</Text>
-            <Button
-              title="cancelar"
-              onPress={() => setModalVisible(false)}
-            ></Button>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              value={selectedItemValue?.toString()}
+              editable={false}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="dd/mm/yyyy"
+              showSoftInputOnFocus={false}
+              caretHidden={true}
+              // value={dataTextInput}
+            />
+            <TextInput
+              style={styles.input}
+              value={selectedItemDescricao?.toString()}
+              editable={false}
+            />
+            <View style={styles.buttonGroup}>
+              <Button title="Editar" color="#4CAF50" />
+              <Button title="Excluir" color="#B22222" />
+              <Button
+                title="Cancelar"
+                onPress={() => setModalVisible(false)}
+                color="#757575"
+              />
+            </View>
           </View>
         </View>
       </Modal>
@@ -140,6 +171,20 @@ const styles = StyleSheet.create({
     shadowOpacity: 5.25,
     shadowRadius: 3.84,
     elevation: 5,
+  },
+  input: {
+    width: "100%",
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#616161",
+    borderRadius: 10,
+    color: "white",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    borderRadius: 20,
+    width: "100%",
   },
 });
 export default ListaDeTransacoes;
