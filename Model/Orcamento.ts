3cfd964b2d06b254c58ef370dde8818c484ec977
Felipe Oliveira <felipeol.est@gmail.com>
2024-04-01 22:26:18 -0300

    alteracao orcamento

--- Model/Orcamento.ts
+++ Model/Orcamento.ts
@@ -4,13 +4,15 @@ export class Orcamento{
     categoria: string;
     descricao?: string;
     valor: number;
+    data: Date;
   
-    constructor(usuarioId: string,id: string, categoria: string, descricao: string, valor: number) {
+    constructor(usuarioId: string,id: string, categoria: string, descricao: string, valor: number, data: Date) {
       this.id = id; 
       this.usuarioId = usuarioId; 
       this.categoria = categoria;
       this.descricao = descricao; 
       this.valor = valor; 
+      this.data = data;
     }
   }
   
