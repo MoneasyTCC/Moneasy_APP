255cf39b8ce5db806c92bbf9b6e7dce0a8db567b
TCC <tccapp.financeiro@gmail.com>
2024-04-08 16:17:46 -0300

    Lista Orçamentos Zika demais

--- src/screens/Menu/Menu.tsx
+++ src/screens/Menu/Menu.tsx
@@ -1,71 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  Button,
-  StyleSheet,
-  Image,
-  Modal,
-  TextInput,
-  Alert,
   TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+  Platform,
 } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../../shared/config";
 import NavigationBar from "../menuNavegation";
+import { Orcamento } from "../../../Model/Orcamento";
+import { obterSaldoPorMes, obterOrcamentosPorData } from "../../../Controller/OrcamentoController";
 
-type OrcamentoScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  "Orcamento"
->;
+type OrcamentoScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Orcamento">;
 
 type Props = {
   navigation: OrcamentoScreenNavigationProp;
 };
 
-// Use as props na definição do seu componente
 export default function MenuScreen({ navigation }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
   const [dataSelecionada, setDataSelecionada] = useState(new Date());
+  const [saldo, setSaldo] = useState<number | null>(null);
+  const [orcamentos, setOrcamentos] = useState<Orcamento[]>([]);
   const [monthIndex, setMonthIndex] = useState(dataSelecionada.getMonth());
   const monthNames = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro",
   ];
+
+  const onChange = (event: any, selectedDate?: Date) => {
+    const currentDate = selectedDate || dataSelecionada;
+    setDataSelecionada(currentDate);
+    setIsLoading(true);
+    obterDados(currentDate);
+  };
+
+  const obterDados = async (date: Date) => {
+    try {
+      const saldoPorMes = await obterSaldoPorMes(date);
+      setSaldo(saldoPorMes.saldo);
+      const orcamentosPorData = await obterOrcamentosPorData(date);
+      console.log(JSON.stringify(orcamentosPorData));
+      setOrcamentos(orcamentosPorData);
+    } catch (error) {
+      console.error("Erro ao buscar saldo por mês:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+    useEffect(() => {
+      obterDados(dataSelecionada);
+    }, [dataSelecionada]);
 
   const updateMonth = (newMonthIndex: number) => {
     setMonthIndex(newMonthIndex);
     const newData = new Date(dataSelecionada.getFullYear(), newMonthIndex, 1);
     setDataSelecionada(newData);
-    /* updateSaldo(newData); */
   };
 
   const handlePreviousMonth = () => {
     const newMonthIndex = monthIndex > 0 ? monthIndex - 1 : 11;
     updateMonth(newMonthIndex);
+    obterDados(new Date(dataSelecionada.getFullYear(), newMonthIndex, 1));
   };
 
   const handleNextMonth = () => {
     const newMonthIndex = monthIndex < 11 ? monthIndex + 1 : 0;
     updateMonth(newMonthIndex);
+    obterDados(new Date(dataSelecionada.getFullYear(), newMonthIndex, 1));
   };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textOrcamento}>Orçamento</Text>
       <View style={styles.menuHeader}>
-        <TouchableOpacity
-          onPress={handlePreviousMonth}
-          style={styles.arrowButton}
-        >
+        <TouchableOpacity onPress={handlePreviousMonth} style={styles.arrowButton}>
           <Text style={styles.arrowText}>&lt;</Text>
         </TouchableOpacity>
         <Text style={styles.mesLabel}>{monthNames[monthIndex]}</Text>
@@ -73,7 +83,21 @@ export default function MenuScreen({ navigation }: Props) {
           <Text style={styles.arrowText}>&gt;</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.menuBody}></View>
+      <View style={styles.menuBody}>
+        <View style={styles.content}>
+          <View style={styles.listaContainer}>
+            {orcamentos.map((orcamento, index) => (
+              <View key={index} style={styles.itemContainer}>
+                <Text style={styles.itemText}>{/* Exibir os detalhes do orçamento aqui */}</Text>
+                 <Text style={styles.itemText}>Categoria: {orcamento.categoria}</Text>
+                      <Text style={styles.itemText}>Descrição: {orcamento.descricao || "Sem descrição"}</Text>
+                      <Text style={styles.itemText}>Valor Atual: R$ {orcamento.valorAtual.toFixed(2)}</Text>
+                      <Text style={styles.itemText}>Valor Definido: R$ {orcamento.valorDefinido.toFixed(2)}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
       <View style={styles.menuFooter}>
         <NavigationBar />
       </View>
@@ -87,23 +111,33 @@ const styles = StyleSheet.create({
     backgroundColor: "#2B2B2B",
   },
   menuHeader: {
+    overflow: "scroll",
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
     flexDirection: "row",
-    alignItems: "flex-end",
+    alignItems: "center",
     justifyContent: "center",
     width: "100%",
-    height: "16%",
+    height: "20%",
+    backgroundColor: "#3A3E3A",
   },
   menuBody: {
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
     width: "100%",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     paddingVertical: 20,
+  },
+  content: {
+    borderRadius: 40,
+    width: "90%",
+    flex: 1,
     backgroundColor: "#3A3E3A",
+    padding: 20,
   },
   menuFooter: {
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
@@ -111,29 +145,46 @@ const styles = StyleSheet.create({
     height: "15%",
     backgroundColor: "#3A3E3A",
   },
-  textOrcamento: {
-    position: "absolute",
-    marginTop:35,
-    marginLeft:20,
-    fontSize: 26,
-    color: "#ffffff",
-    fontWeight: "bold",
+  arrowButton: {
+    /* padding: 10 */
   },
-  /* Select dos meses */
-
-  arrowButton: { marginBottom:10},
   arrowText: {
     color: "#ffffff",
     fontSize: 30,
     fontWeight: "bold",
     lineHeight: 30,
   },
+  saldoBody: {
+    alignItems: "center",
+  },
   mesLabel: {
     color: "#ffffff",
     width: "35%",
     textAlign: "center",
     fontSize: 26,
-    marginBottom:10
   },
-  /* Fim Select dos meses */
+  saldoText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#ffffff",
+  },
+  saldoAtual: {
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "#ffffff",
+  },
+  listaContainer: {
+    flex: 1,
+    marginTop: 20,
+  },
+  itemContainer: {
+    backgroundColor: "#3A3E3A",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  itemText: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
 });
