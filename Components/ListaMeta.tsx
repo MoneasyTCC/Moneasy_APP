7b65ffaf9024749bc0476843953a0c2b0e439d5c
joaogvt <sotorivarossi@gmail.com>
2024-04-18 15:12:53 -0300

    estilização da lista de metas

--- Components/ListaMeta.tsx
+++ Components/ListaMeta.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, StyleSheet, Alert, TouchableOpacity, Image } from "react-native";
 import { obterMetasPorData } from "../Controller/MetaController";
 import { MetasDAL } from "../Repo/RepositorioMeta";
 import { Meta } from "../Model/Meta";
@@ -76,35 +76,123 @@ const ListaDeMetas: React.FC<ListaDeMetasProps> = ({ dataSelecionada }) => {
       }
     >
       <View style={styles.container}>
-        <Text>{item.titulo}</Text>
-        <Text>{item.valorAtual}</Text>
-        <Text>{item.valorObjetivo}</Text>
-        <Text>
-          Começa em:{" "}
-          {converterTimestampParaData(item.dataInicio?.toString()).toLocaleDateString("pt-br")}
-        </Text>
-        <Text>
-          Finaliza em:{" "}
-          {converterTimestampParaData(item.dataFimPrevista?.toString()).toLocaleDateString("pt-br")}
-        </Text>
+        <View style={{ width: "100%", justifyContent: "flex-start" }}>
+          <View style={{ flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
+            <Text style={styles.text}>{item.titulo}</Text>
+            <TouchableOpacity>
+              <Image source={require("../assets/hamburguerMenu.png")} />
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "100%", flexDirection: "row", justifyContent: "space-between" }}>
+            <Text style={styles.textOpaco}>{item.status}</Text>
+            <Text style={styles.textOpaco}>tempo em: dia</Text>
+          </View>
+        </View>
+        <View style={styles.valoresContainer}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Text style={styles.text}>Valor guardado</Text>
+            <Text style={styles.textValor}>R${item.valorAtual},00</Text>
+          </View>
+          <View style={styles.separador}></View>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Text style={styles.text}>Valor Objetivo</Text>
+            <Text style={styles.textValor}>R${item.valorObjetivo},00</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: 10,
+          }}
+        >
+          <Text style={{ fontSize: 12, color: "#fff", fontWeight: "bold" }}>
+            Meta 50% concluida
+          </Text>
+          <Text style={{ fontSize: 12, color: "#fff", fontWeight: "bold" }}>
+            Finaliza em:{" "}
+            {converterTimestampParaData(item.dataFimPrevista?.toString()).toLocaleDateString(
+              "pt-br"
+            )}
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
   return (
-    <FlatList
-      data={metas}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={renderItem}
-    />
+    <>
+      <View
+        style={{
+          flexDirection: "row",
+          width: "90%",
+          height: 50,
+          backgroundColor: "#2a2a2a",
+          borderRadius: 30,
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          marginBottom: 20,
+        }}
+      >
+        <TouchableOpacity>
+          <Text style={{ color: "#0fec32" }}>Ativo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Pausado</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Concluido</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        style={{ width: "90%" }}
+        data={metas}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <View style={{ marginBottom: 10 }} />}
+      />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    padding: 10,
     flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
+    backgroundColor: "#3a3e3a",
+    //backgroundColor: "#2a2a2a",
+    elevation: 5,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+    borderWidth: 1,
+    borderRadius: 15,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  textOpaco: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+    opacity: 0.7,
+  },
+  textValor: {
+    color: "#0fec32",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  separador: {
+    backgroundColor: "#656865",
+    width: 1,
+  },
+  valoresContainer: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
   },
 });
 
